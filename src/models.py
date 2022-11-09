5306097155dc45da2562dafec38fdbf54443c976
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-11-09 23:53:01 +0000

    Project done

--- src/models.py
+++ src/models.py
@@ -29,5 +29,52 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False) 
+    firstname =Column (String(250),nullable=False)
+    lastname=Column (String(250),nullable=False)
+    email = Column(String(250), nullable=False)
+    comments=relationship ("Comment")
+    posts=relationship ("Post")
+    followers=relationship ("Follower")
+## Draw from SQLAlchemy base
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id=Column(Integer,ForeignKey("user.id"))
+    user_to_id =Column(Integer, ForeignKey("user.id"))
+
+
+class Comment (Base):
+    __tablename__ = 'comment' 
+    id = Column(Integer, primary_key=True)
+    comment_text=Column(String(250), nullable=False)
+    author_id=Column(Integer,ForeignKey("user.id"))
+    post_id=Column(Integer,ForeignKey("post.id"))
+
+
+class Post (Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id=Column(Integer,ForeignKey("user.id"))
+    comments=relationship ("Comment")
+    medias=relationship ("Media")
+
+class Media(Base):
+    __tablename__ = 'media' 
+    id = Column(Integer, primary_key=True)
+    url=Column (String(250),nullable=False)
+    post_id=Column(Integer,ForeignKey("post.id"))
+
+
+
+
+
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
